9da4b7e0764bcfb7649c7a4431ac4b17a67d2a53
justnicko <kolya.averchenko@gmail.com>
2024-06-19 18:29:06 +0300

    bo

--- src/main/java/net/nicko/colonist/ColonistDataGenerator.java
+++ src/main/java/net/nicko/colonist/ColonistDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ColonistDataGenerator implements DataGeneratorEntrypoint {
+	//jkj
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
