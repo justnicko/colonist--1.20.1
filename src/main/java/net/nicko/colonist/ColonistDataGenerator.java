68fad80fc622b108c93d4bea9c5c46a2e2641a70
justnicko <kolya.averchenko@gmail.com>
2024-06-19 18:24:05 +0300

    Initial commit

---
+++ src/main/java/net/nicko/colonist/ColonistDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nicko.colonist;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ColonistDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/nicko/colonist/ColonistModClient.java
@@ -0,0 +1,11 @@
+package net.nicko.colonist;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColonistModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
